3f39b6c3ea708dba7de2adef21b1f37a4100b5a1
Ejamador08 <eliecer.jar2022@gmail.com>
2022-11-18 16:22:13 -0600

    Sitio Oficial IAFCJ Nicaragua - Ing. Eliecer Amador

---
+++ Contents/js/Reflexion.js
@@ -0,0 +1,38 @@
+function MiReflexion() {}
+MiReflexion = new MiReflexion();
+number = 0;
+// textArray
+MiReflexion[number++] =
+  " <br><br><b> --  --</b>";
+  MiReflexion[number++] =
+  " <br><br><b> --  --</b>";
+  MiReflexion[number++] =
+  " <br><br><b> --  --</b>";
+  MiReflexion[number++] =
+  " <br><br><b> --  --</b>";
+  MiReflexion[number++] =
+  " <br><br><b> --  --</b>";
+  MiReflexion[number++] =
+  " <br><br><b> --  --</b>";
+  MiReflexion[number++] =
+  " <br><br><b> --  --</b>";
+  MiReflexion[number++] =
+  " <br><br><b> --  --</b>";
+  MiReflexion[number++] =
+  " <br><br><b> --  --</b>";
+  MiReflexion[number++] =
+  " <br><br><b> --  --</b>";
+  MiReflexion[number++] =
+  " <br><br><b> --  --</b>";
+  MiReflexion[number++] =
+  " <br><br><b> --  --</b>";
+  MiReflexion[number++] =
+  " <br><br><b> --  --</b>";
+  MiReflexion[number++] =
+  " <br><br><b> --  --</b>";
+  MiReflexion[number++] =
+  " <br><br><b> --  --</b>";
+  MiReflexion[number++] =
+  " <br><br><b> --  --</b>";
+increment = Math.floor(Math.random() * number);
+document.write(MiReflexion[increment]);
---
+++ Contents/js/Versiculo.js
@@ -0,0 +1,70 @@
+function misFrases() {}
+misFrases = new misFrases();
+number = 0;
+// textArray
+misFrases[number++] =
+  "Pedro les dijo: Arrepentíos, y bautícese cada uno de vosotros en el nombre de Jesucristo para perdón de los pecados; y recibiréis el don del Espíritu Santo. <br><br><b> -- Hechos 2:38 --</b>";
+misFrases[number++] =
+  "Edificados sobre el fundamento de los apóstoles y profetas, siendo la principal piedra del ángulo Jesucristo mismo.<br><br><b> -- Efesios 2:20 --</b>";
+misFrases[number++] =
+  "Oye, Israel: Jehová nuestro Dios, Jehová uno es.<br><br><b> -- Deuteronomio 6:4 --</b>";
+misFrases[number++] =
+  "Y amarás a Jehová tu Dios de todo tu corazón, y de toda tu alma, y con todas tus fuerzas.<br><br><b> -- Deuteronomio 6:5 --</b>";
+misFrases[number++] =
+  "No te harás imagen, ni ninguna semejanza de lo que esté arriba en el cielo, ni abajo en la tierra, ni en las aguas debajo de la tierra.<br><br><b>-- Exodo 20:4 --</b>";
+misFrases[number++] =
+  "La cordura del hombre detiene su furor, Y su honra es pasar por alto la ofensa.<br><br><b> -- Proverbios 19:11 --</b>";
+misFrases[number++] =
+  "Nunca se apartará de tu boca este libro de la ley, sino que de día y de noche meditarás en él, para que guardes y hagas conforme a todo lo que en él está escrito; porque entonces harás prosperar tu camino, y todo te saldrá bien.<br><br><b> -- Josué 1:9 --</b>";
+misFrases[number++] =
+  "La cordura del hombre detiene su furor, Y su honra es pasar por alto la ofensa.<br><br><b> -- Proverbios 19:11 --</b>";
+misFrases[number++] =
+  "Y esta es la confianza que tenemos en él, que si pedimos alguna cosa conforme a su voluntad, él nos oye.<br><br><b> -- 1 Juan 5:14 --</b>";
+misFrases[number++] =
+  "En esto conocemos que amamos a los hijos de Dios, cuando amamos a Dios, y guardamos sus mandamientos.<br><br><b> -- 1 Juan 5:2 --</b>";
+misFrases[number++] =
+  "Amado, no imites lo malo, sino lo bueno. El que hace lo bueno es de Dios; pero el que hace lo malo, no ha visto a Dios.<br><br><b> -- 3 Juan 1:11 --</b>";
+misFrases[number++] =
+  "Señor, digno eres de recibir la gloria y la honra y el poder; porque tú creaste todas las cosas, y por tu voluntad existen y fueron creadas.<br><br><b> -- Apocalipsis 4:11 --</b>";
+misFrases[number++] =
+  "E indiscutiblemente, grande es el misterio de la piedad: Dios fue manifestado en carne, Justificado en el Espíritu, Visto de los ángeles, Predicado a los gentiles, Creído en el mundo, Recibido arriba en gloria.<br><br><b> -- 1 Timoteo 3:16 --</b>";
+misFrases[number++] =
+  "Y el mismo Dios de paz os santifique por completo; y todo vuestro ser, espíritu, alma y cuerpo, sea guardado irreprensible para la venida de nuestro Señor Jesucristo.<br><br><b> -- 1 Tesalonicenses 5:23 --</b>";
+misFrases[number++] =
+  "Porque hay un solo Dios, y un solo mediador entre Dios y los hombres, Jesucristo hombre.<br><br><b> -- 1 Timoteo 2:5 --</b>";
+misFrases[number++] =
+  "Asimismo que las mujeres se atavíen de ropa decorosa, con pudor y modestia; no con peinado ostentoso, ni oro, ni perlas, ni vestidos costosos, sino con buenas obras, como corresponde a mujeres que profesan piedad.<br><br><b> -- 1 Timoteo 2:9-10 --</b>";
+misFrases[number++] =
+  "Pero sabemos que el Hijo de Dios ha venido, y nos ha dado entendimiento para conocer al que es verdadero; y estamos en el verdadero, en su Hijo Jesucristo. Este es el verdadero Dios, y la vida eterna.<br><br><b> -- 1 Juan 5:20 --</b>";
+misFrases[number++] =
+  "Digo, pues: Andad en el Espíritu, y no satisfagáis los deseos de la carne.<br><br><b> -- Galatas 5:16 --</b>";
+misFrases[number++] =
+  "Porque vosotros, hermanos, a libertad fuisteis llamados; solamente que no uséis la libertad como ocasión para la carne, sino servíos por amor los unos a los otros.<br><br><b> -- Galatas 5:13 --</b>";
+misFrases[number++] =
+  "Un poco de levadura leuda toda la masa.<br><br><b> -- Galatas 5:9 --</b>";
+misFrases[number++] =
+  "No os engañéis; Dios no puede ser burlado: pues todo lo que el hombre sembrare, eso también segará.<br><br><b>-- Galatas 6:7 --</b>";
+misFrases[number++] =
+  "Así que, según tengamos oportunidad, hagamos bien a todos, y mayormente a los de la familia de la fe.<br><br><b> -- Galatas 6:10 --</b>";
+misFrases[number++] =
+  "No nos cansemos, pues, de hacer bien; porque a su tiempo segaremos, si no desmayamos.<br><br><b> -- Galatas 6:9 --</b>";
+misFrases[number++] =
+  "Es, pues, la fe la certeza de lo que se espera, la convicción de lo que no se ve.<br><br><b> -- Hebreos 11:1 --</b>";
+misFrases[number++] =
+  "Porque: El que quiere amar la vida Y ver días buenos, Refrene su lengua de mal, Y sus labios no hablen engaño;<br><br><b> -- 1 Pedro 3:10 --</b>";
+misFrases[number++] =
+  "Porque los ojos del Señor están sobre los justos, Y sus oídos atentos a sus oraciones; Pero el rostro del Señor está contra aquellos que hacen el mal.<br><br><b> -- 1 Pedro 3:12 --</b>";
+misFrases[number++] =
+  "Porque por gracia sois salvos por medio de la fe; y esto no de vosotros, pues es don de Dios; no por obras, para que nadie se gloríe.<br><br><b> -- 1 Efesios 2:8-9 --</b>";
+misFrases[number++] =
+  "¿No sabéis que los injustos no heredarán el reino de Dios? No erréis; ni los fornicarios, ni los idólatras, ni los adúlteros, ni los afeminados, ni los que se echan con varones, ni los ladrones, ni los avaros, ni los borrachos, ni los maldicientes, ni los estafadores, heredarán el reino de Dios.<br><b> -- 1 Corintios 6:9-10 --</b>";
+misFrases[number++] =
+  "Mas a todos los que le recibieron, a los que creen en su nombre, les dio potestad de ser hechos hijos de Dios;<br><br><b> -- Juan 1:12 --</b>";
+misFrases[number++] =
+  "Porque las cosas invisibles de él, su eterno poder y deidad, se hacen claramente visibles desde la creación del mundo, siendo entendidas por medio de las cosas hechas, de modo que no tienen excusa.<br><br><b> -- Romanos 1:20 --</b>";
+misFrases[number++] =
+  "Riquezas, honra y vida son la remuneración de la humildad y del temor de Jehová.<br><br><b> -- Proverbios 22:4 --</b>";
+misFrases[number++] =
+  "Todo lo puedo en Cristo que me fortalece.<br><br><b> -- Filipenses 4:13 --</b>";
+increment = Math.floor(Math.random() * number);
+document.write(misFrases[increment]);
